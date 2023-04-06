9cb978f493bc23ef614dba3c4abbefc875dd7f15
amol <amlo1232001@gmail.com>
2023-04-07 01:17:09 +0530

    feat: #1 add layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
 import Layout from "./layouts/Layout";
 import "./app.css";
+import ContactGrid from "./components/ContactGrid";
+
+const test = [
+  {
+    id: 1,
+    name: "test",
+    email: "test@gmail.com",
+    rating: 5,
+  },
+  {
+    id: 2,
+    name: "amol",
+    email: "amol@gmail.com",
+    rating: 4,
+  },
+];
 
 function App() {
-  return <Layout></Layout>;
+  return (
+    <Layout>
+      <ContactGrid data={test} />
+    </Layout>
+  );
 }
 
 export default App;
---
+++ src/components/ContactGrid.jsx
@@ -0,0 +1,15 @@
+import { useMemo } from "react";
+import Contact from "./Contact";
+import { Stack } from "@mui/material";
+
+export default function ContactGrid({ data }) {
+  const contacts = useMemo(() => {
+    return data.map((item) => <Contact data={item} key={item.id} />);
+  }, [data]);
+
+  return (
+    <Stack sx={{ mx: 2 }} spacing={2}>
+      {contacts}
+    </Stack>
+  );
+}
